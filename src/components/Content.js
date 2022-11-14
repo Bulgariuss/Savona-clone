424cc8e23c597b38134a006c2b350692cc7af22b
Kuba Szczęsnowicz <kubalek00@gmail.com>
2022-11-14 20:21:52 +0100

    Add banner and footer

--- src/components/Content.js
+++ src/components/Content.js
@@ -4,6 +4,8 @@ import header from '../img/headerfb.png'
 import promobanner from '../img/promobanner.jpg'
 import locationbanner from '../img/locationbanner.jpg'
 import bbqbanner from '../img/bbqbanner.png'
+import logoSmall from '../img/savona-logo-small.png'
+import fblogo from '../img/fblogo.png'
 
 
 const allNews = [{
@@ -47,7 +49,18 @@ const Content = () => {
                 </div>
               ) )}
             </div>
+            <div className="fbBanner">
+              <div className="fbBannerBlur"></div>
+              <a href='https://www.facebook.com/savona.pizzaclub/' alt="" className="fbLogo" target="_blank"/>
+              <a href='https://www.facebook.com/savona.pizzaclub/' className="bannerName" target="_blank">Savona PizzaClub</a>
+              <h2 className="bannerFollows">5600 obserwujący</h2>
+              <a href='https://www.facebook.com/savona.pizzaclub/' className="follow" target="_blank"></a>
+              <a href='https://www.facebook.com/savona.pizzaclub/' className="share" target="_blank"></a>
+            </div>
           </div>
+        </div>
+        <div className="space">
+          <img src={logoSmall} alt="" />
         </div>
     </div>
   )
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,37 @@ import './Footer.css'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <div className="footer">
+      <div className="footerContent">
+        <div className="footerTop">
+          <div className="newsletter">
+            <div className="footerDivHeader"></div>
+            <form>
+              <input type="text" className="footerInput" placeholder='Twój adres e-mail' />
+              <button className="footerBtn"></button>
+            </form>
+          </div>
+          <div className="recommend">
+            <div className="footerDivHeader"></div>
+            <form>
+              <input type="text" className="footerInput" placeholder='Adres e-mail znajomego' />
+              <input type="text" className="footerInput" placeholder='Twoje imię' />
+              <button className="footerBtn"></button>
+            </form>
+          </div>
+          <div className="important">
+            <div className="footerDivHeader"></div>
+            <a href="#" className="importantElement">Kontakt</a>
+          </div>
+        </div>
+        <div className="footerBot">
+          Wszelkie prawa zastrzeżone © 2008-2020 dla Savona
+          <br />
+          <a href="http://www.epro.com.pl/" target="_blank" className="footerA">Strony internetowe</a>
+          <a href="http://www.epro.com.pl/" target="_blank" className="footerLogo footerA"></a>
+        </div>
+      </div>
+    </div>
   )
 }
 
